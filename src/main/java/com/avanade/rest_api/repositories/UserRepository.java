4d7375f5eeca7f65288fedf51f8cf333b2a20428
Danilozxz <progdanilo@gmail.com>
2025-02-26 21:35:36 -0300

    feat: add Repository de User

---
+++ src/main/java/com/avanade/rest_api/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package com.avanade.rest_api.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.avanade.rest_api.models.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>{
+
+    
+}
