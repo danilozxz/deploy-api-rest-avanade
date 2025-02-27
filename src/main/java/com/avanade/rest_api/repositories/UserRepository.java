dbfb84af4e78abbcc180a0f7811d7bee36a0e50c
Danilozxz <progdanilo@gmail.com>
2025-02-27 18:46:39 -0300

    feat: add busca no Repository de User

--- src/main/java/com/avanade/rest_api/repositories/UserRepository.java
+++ src/main/java/com/avanade/rest_api/repositories/UserRepository.java
@@ -8,5 +8,5 @@ import com.avanade.rest_api.models.User;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long>{
 
-    
+    boolean existsByAccountNumber(String accountNumber);
 }
